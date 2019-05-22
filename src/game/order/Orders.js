5a7748c643244bb6d4cc8de17dda3b0e89df63ed
sozemego <piknypiec@gmail.com>
2019-05-22 20:14:10 +0200

    Transitions for Orders

--- src/game/order/Orders.js
+++ src/game/order/Orders.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTransition, animated } from "react-spring";
 import { Order } from "./Order";
 import { getOrders, getTakenOrderIds } from "../selectors";
 import { Card } from "antd";
@@ -10,10 +11,18 @@ export function Orders(props) {
 
   const takenOrders = takenOrderIds.map(id => orders[id]);
 
+  const transitions = useTransition(takenOrders, order => order.id, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
   return (
     <Card title={<OrderTitle />} style={{ minHeight: "90vh" }}>
-      {takenOrders.map(order => (
-        <Order order={order} key={order.id} />
+      {transitions.map(({ item, key, props }) => (
+        <animated.div key={key} style={props}>
+          <Order order={item} />
+        </animated.div>
       ))}
     </Card>
   );
