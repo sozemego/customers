037bc0912e1013c0b659dd7d4f91e6e35b8ac3f6
sozemego <piknypiec@gmail.com>
2019-05-25 00:16:53 +0200

    Work into level editor

--- src/levelEditor/LevelEditor.js
+++ src/levelEditor/LevelEditor.js
@@ -1,18 +1,125 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useState } from "react";
+import { Button, Input } from "antd";
 import { redirect } from "../history";
+import { css } from "glamor";
+import faker from "faker";
+
+const buttonContainerStyle = css({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center"
+});
+
+const nameContainerStyle = css({
+  margin: "4px",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center"
+});
+
+const addCustomerButtonStyle = css({
+  margin: "4px",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center"
+});
+
+const customersContainerStyle = css({
+  margin: "4px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center"
+});
+
+const customerContainer = css({
+  margin: "4px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+	border: "1px dotted gray"
+});
+
+let id = 0;
 
 export function LevelEditor(props) {
+  const [name, setName] = useState(null);
+  const [customers, setCustomers] = useState([]);
 
+  console.log("name", name);
+  console.log("customers", customers);
 
-	return (
-		<div>
-			<div >
-				<Button onClick={() => redirect("/")}>
-					Back
-				</Button>
-			</div>
-			Here be level editor
-		</div>
-	)
+  return (
+    <div>
+      <div className={buttonContainerStyle}>
+        <Button type={"danger"} onClick={() => redirect("/")}>
+          Back
+        </Button>
+        <Button type={"danger"}>Reset level</Button>
+      </div>
+      <div className={nameContainerStyle}>
+        <Input
+          placeholder={"Level name"}
+          value={name}
+          allowClear
+          addonBefore={"Level name"}
+          onChange={e => {
+            setName(e.target.value);
+          }}
+          style={{ width: "25%" }}
+        />
+      </div>
+      <div className={addCustomerButtonStyle}>
+        <Button
+          type={"danger"}
+          onClick={() => {
+            setCustomers([
+              ...customers,
+              { id: ++id, name: faker.name.firstName() }
+            ]);
+          }}
+        >
+          Add customer
+        </Button>
+      </div>
+      <div className={customersContainerStyle}>
+        {customers.map(customer => {
+          return (
+            <div key={customer.id} className={customerContainer}>
+              <div>
+                <Input
+                  value={customer.name}
+                  allowClear
+                  onChange={e => {
+                    const index = customers.findIndex(
+                      c => c.id === customer.id
+                    );
+                    customers[index].name = e.target.value;
+                    setCustomers([...customers]);
+                  }}
+                />
+              </div>
+              <div>
+                <Button
+                  type={"danger"}
+                  onClick={() => {
+                    const index = customers.findIndex(
+                      c => c.id === customer.id
+                    );
+                    customers.splice(index, 1);
+                    setCustomers([...customers]);
+                  }}
+                >
+                  Remove customer
+                </Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div />
+    </div>
+  );
 }
